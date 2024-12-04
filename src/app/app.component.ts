d2a08fb745dabcbd4e376153d5611ae6ad826aff
gitalvininfo <yanson.alvin.info@gmail.com>
2024-12-04 14:37:38 +0800

    done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,10 @@
 import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
-import { UseClassComponent } from './use-class/use-class.component';
-import { UseValueComponent } from './use-value/use-value.component';
-import { UseFactoryComponent } from "./use-factory/use-factory.component";
-import { UseExistingComponent } from './use-existing/use-existing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ReactiveFormsModule, UseClassComponent, UseValueComponent, UseFactoryComponent, UseExistingComponent],
+  imports: [ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
